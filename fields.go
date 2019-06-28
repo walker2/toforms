81d8541e24765d1c8ee328dc38dc98602f5e2f1e
Andrew Shipilo <andrewshipilo@mail.ru>
2019-06-28 11:48:32 +0300

    Tests for unexported fields, pointers (and nil*), nested structs and invalid types

--- fields.go
+++ fields.go
@@ -1,14 +1,70 @@
 package toforms
 
+import (
+	"reflect"
+)
+
 type field struct {
 	Label       string
 	Name        string
 	Type        string
 	Placeholder string
+	Value       interface{}
 }
 
-func fields(strct interface{}) field {
-	return field{}
+func valueOf(v interface{}) reflect.Value {
+	var rv reflect.Value
+	switch value := v.(type) {
+	case reflect.Value:
+		rv = value
+	default:
+		rv = reflect.ValueOf(v)
+	}
+
+	if rv.Kind() == reflect.Ptr {
+		// If pointer is nil we should create new copy of underlying type
+		if rv.IsNil() {
+			rv = reflect.New(rv.Type().Elem())
+		}
+		// Dereference pointer
+		rv = rv.Elem()
+	}
+	return rv
+}
+
+func fields(strct interface{}) []field {
+	rv := valueOf(strct)
+	if rv.Kind() != reflect.Struct {
+		panic("Form: only structs are supported")
+	}
+
+	t := rv.Type()
+	var ret []field
+
+	for i := 0; i < t.NumField(); i++ {
+		tf := t.Field(i)
+		rvf := valueOf(rv.Field(i))
+		if !rvf.CanInterface() {
+			continue
+		}
+		if rvf.Kind() == reflect.Struct {
+			nestedFields := fields(rvf.Interface())
+			for i, nf := range nestedFields {
+				nestedFields[i].Name = tf.Name + "." + nf.Name
+			}
+			ret = append(ret, nestedFields...)
+			continue
+		}
+		f := field{
+			Label:       tf.Name,
+			Name:        tf.Name,
+			Type:        "text",
+			Placeholder: tf.Name,
+			Value:       rvf.Interface(),
+		}
+		ret = append(ret, f)
+	}
+	return ret
 }
 
 // func HTML(strct interface{}, tpl *template.Template) template.HTML {
--- fields_test.go
+++ fields_test.go
@@ -2,32 +2,368 @@ package toforms
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 func TestFields(t *testing.T) {
-	tests := []struct {
+	var nilStructPtr *struct {
+		Name string
+		Age  int
+	} = nil
+
+	tests := map[string]struct {
 		strct interface{}
-		want  field
+		want  []field
 	}{
-		{
+		"Simplest use case": {
 			strct: struct {
 				Name string
 			}{},
-			want: field{
-				Label:       "Name",
-				Name:        "Name",
-				Type:        "text",
-				Placeholder: "Name",
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "",
+				},
+			},
+		},
+		"Multiple fields should be supported": {
+			strct: struct {
+				Name  string
+				Email string
+				Age   int
+			}{},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "",
+				},
+				{
+					Label:       "Email",
+					Name:        "Email",
+					Type:        "text",
+					Placeholder: "Email",
+					Value:       "",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       0,
+				},
+			},
+		},
+		"Values should be parsed": {
+			strct: struct {
+				Name  string
+				Email string
+				Age   int
+			}{
+				Name:  "John Walker",
+				Email: "jon@mail.com",
+				Age:   23,
+			},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "John Walker",
+				},
+				{
+					Label:       "Email",
+					Name:        "Email",
+					Type:        "text",
+					Placeholder: "Email",
+					Value:       "jon@mail.com",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       23,
+				},
+			},
+		},
+		"Unexported fields should be skipped": {
+			strct: struct {
+				Name  string
+				email string
+				Age   int
+			}{
+				Name:  "John Walker",
+				email: "jon@mail.com",
+				Age:   23,
+			},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "John Walker",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       23,
+				},
+			},
+		},
+		"Pointers to structs should be supported": {
+			strct: &struct {
+				Name  string
+				email string
+				Age   int
+			}{
+				Name: "John Walker",
+				Age:  23,
+			},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "John Walker",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       23,
+				},
+			},
+		},
+		"Nil pointers with a struct type should be supported": {
+			strct: nilStructPtr,
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       0,
+				},
+			},
+		},
+		"Pointer fields should be supported": {
+			strct: struct {
+				Name *string
+				Age  *int
+			}{},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       0,
+				},
+			},
+		},
+		"Nested structs should be supported": {
+			strct: struct {
+				Name    string
+				Address struct {
+					Street string
+					Zip    int
+				}
+			}{
+				Name: "Jon Walker",
+				Address: struct {
+					Street string
+					Zip    int
+				}{
+					Street: "123 Some St",
+					Zip:    1234,
+				},
+			},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "Jon Walker",
+				},
+				{
+					Label:       "Street",
+					Name:        "Address.Street",
+					Type:        "text",
+					Placeholder: "Street",
+					Value:       "123 Some St",
+				},
+				{
+					Label:       "Zip",
+					Name:        "Address.Zip",
+					Type:        "text",
+					Placeholder: "Zip",
+					Value:       1234,
+				},
+			},
+		},
+		"Doubly-Nested structs should be supported": {
+			strct: struct {
+				A struct {
+					B struct {
+						C1 string
+						C2 int
+					}
+				}
+			}{
+				A: struct {
+					B struct {
+						C1 string
+						C2 int
+					}
+				}{
+					B: struct {
+						C1 string
+						C2 int
+					}{
+						C1: "string",
+						C2: 123,
+					},
+				},
+			},
+			want: []field{
+				{
+					Label:       "C1",
+					Name:        "A.B.C1",
+					Type:        "text",
+					Placeholder: "C1",
+					Value:       "string",
+				},
+				{
+					Label:       "C2",
+					Name:        "A.B.C2",
+					Type:        "text",
+					Placeholder: "C2",
+					Value:       123,
+				},
 			},
 		},
 	}
 
-	for _, tc := range tests {
-		t.Run(fmt.Sprintf("%v", tc.strct), func(t *testing.T) {
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
 			got := fields(tc.strct)
-			if got != tc.want {
+			if !reflect.DeepEqual(got, tc.want) {
 				t.Errorf("fields() = %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestFields_invalidTypes(t *testing.T) {
+	tests := []struct {
+		notAStruct interface{}
+	}{
+		{"this is a string"},
+		{123},
+		{nil},
+	}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%T", tc.notAStruct), func(t *testing.T) {
+			defer func() {
+				if err := recover(); err == nil {
+					t.Errorf("fields(%v) did not panic", tc.notAStruct) // fail
+				}
+			}()
+			fields(tc.notAStruct)
+		})
+	}
+}
+
+func TestFields_label(t *testing.T) {
+	hasLabels := func(labels ...string) func(*testing.T, []field) {
+		return func(t *testing.T, fields []field) {
+			if len(fields) != len(labels) {
+				t.Fatalf("fields() len = %d; want %d", len(fields), len(labels))
+			}
+			for i := 0; i < len(fields); i++ {
+				if fields[i].Label != labels[i] {
+					t.Errorf("fields()[%d].Label = %s; want %s", i, fields[i].Label, labels[i])
+				}
+			}
+		}
+	}
+	hasValues := func(values ...interface{}) func(*testing.T, []field) {
+		return func(t *testing.T, fields []field) {
+			if len(fields) != len(values) {
+				t.Fatalf("fields() len = %d; want %d", len(fields), len(values))
+			}
+			for i := 0; i < len(fields); i++ {
+				if fields[i].Value != values[i] {
+					t.Errorf("fields()[%d].Value = %v; want %v", i, fields[i].Value, values[i])
+				}
+			}
+		}
+	}
+
+	check := func(checks ...func(*testing.T, []field)) []func(*testing.T, []field) {
+		return checks
+	}
+
+	tests := map[string]struct {
+		strct  interface{}
+		checks []func(*testing.T, []field)
+	}{
+		"No values": {
+			strct: struct {
+				Name string
+			}{},
+			checks: check(hasLabels("Name")),
+		},
+		"Multiple fields with values": {
+			strct: struct {
+				Name  string
+				Email string
+				Age   int
+			}{
+				Name:  "John Walker",
+				Email: "jon@mail.com",
+				Age:   23,
+			},
+			checks: check(
+				hasLabels("Name", "Email", "Age"),
+				hasValues("John Walker", "jon@mail.com", 23),
+			),
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := fields(tc.strct)
+			for _, check := range tc.checks {
+				check(t, got)
 			}
 		})
 	}
